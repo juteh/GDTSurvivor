caf1d777d7e2a6e19ff0e2d06b48b87f317225bf
juteh <leon.adam.91@gmail.com>
2024-10-21 19:43:29 +0200

    add new spaceship pawn with movement and camera

---
+++ Source/ToonTanks/PlayerSpaceShipPawn.cpp
@@ -0,0 +1,110 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerSpaceShipPawn.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+APlayerSpaceShipPawn::APlayerSpaceShipPawn()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+
+	RootComponent = CapsuleComponent;
+
+	SpaceshipMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SpaceshipMesh"));
+	SpaceshipMesh->SetupAttachment(RootComponent);
+	SpaceshipMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	SpaceshipMesh->SetCollisionProfileName(TEXT("PhysicsActor"));
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(CapsuleComponent);
+	CameraBoom->TargetArmLength = 500.0f;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom);
+
+}
+
+void APlayerSpaceShipPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void APlayerSpaceShipPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// if no keys are pressed, reduce speed over time
+	if (!IsMovingVertical && !IsMovingHorizontal)
+	{
+		FVector DecelerationVector = -CurrentVelocity.GetSafeNormal() * Deceleration * DeltaTime;
+		
+		// stop movement
+		if (DecelerationVector.Size() > CurrentVelocity.Size())
+		{
+			CurrentVelocity = FVector::ZeroVector;
+		}
+		// reduce speed
+		else
+		{
+			CurrentVelocity += DecelerationVector;
+		}
+	}
+
+	CurrentVelocity = FMath::Clamp(CurrentVelocity.Size(), 0.0f, MaxSpeed) * CurrentVelocity.GetSafeNormal();
+
+	// CurrentVelocity set new location and CurrentVelocity changed while usin inputs in MoveVertical() and MoveHorizontal()
+	FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+	// true -> use collision
+	SetActorLocation(NewLocation, true);
+
+	// Rotation of SpaceShip 
+	if (!CurrentVelocity.IsNearlyZero()) {
+		FRotator NewRotation = CurrentVelocity.Rotation();
+		SetActorRotation(FMath::RInterpTo(GetActorRotation(), NewRotation, DeltaTime, RotationSpeed));
+	}
+}
+
+void APlayerSpaceShipPawn::SetupPlayerInputComponent(UInputComponent * PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Move Forward / Backward", this, &APlayerSpaceShipPawn::MoveVertical);
+	PlayerInputComponent->BindAxis("Move Right / Left", this, &APlayerSpaceShipPawn::MoveHorizontal);
+}
+
+void APlayerSpaceShipPawn::MoveVertical(float Value)
+{
+	if (Value != 0.0f)
+	{
+		FVector Direction = FVector(1, 0, 0);
+		CurrentVelocity += Direction * Value * Acceleration * GetWorld()->GetDeltaSeconds();
+		IsMovingVertical = true;
+	}
+	else
+	{
+		IsMovingVertical = false;
+	}
+}
+
+void APlayerSpaceShipPawn::MoveHorizontal(float Value)
+{
+	if (Value != 0.0f)
+	{
+		FVector Direction = FVector(0, 1, 0);
+		CurrentVelocity += Direction * Value * Acceleration * GetWorld()->GetDeltaSeconds();
+		IsMovingHorizontal = true;
+	}
+	else
+	{
+		IsMovingHorizontal = false;
+	}
+}
---
+++ Source/ToonTanks/PlayerSpaceShipPawn.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerSpaceShipPawn.generated.h"
+
+UCLASS()
+class TOONTANKS_API APlayerSpaceShipPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	APlayerSpaceShipPawn();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void MoveVertical(float Value);
+	void MoveHorizontal(float Value);
+
+private:
+	bool IsMovingVertical = false;
+	bool IsMovingHorizontal = false;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UCapsuleComponent* CapsuleComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UStaticMeshComponent* SpaceshipMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UCameraComponent* FollowCamera;
+
+
+	// increase speed over time until maxspeed
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Acceleration = 2000.0f;
+
+	// reduce speed while no movement-keys are pressed until zero
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Deceleration = 500.0f;
+
+	// limit fpr acceleration
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float MaxSpeed = 1000.0f;
+
+	// only rotation speed of the ship. Has no effect on movementspeed
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float RotationSpeed = 5.0f;
+
+	FVector CurrentVelocity;
+};
